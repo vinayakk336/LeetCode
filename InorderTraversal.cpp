87d60d2402e8a27d38671fd2f51073a09ff16a96
vinayakk <vinayakk@umich.edu>
2021-02-12 11:17:00 -0800

    Inorder recursive implementation

--- InorderTraversal.cpp
+++ InorderTraversal.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -11,6 +13,42 @@ struct TreeNode {
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+void inorderHelper(TreeNode* root, vector<int> &trav);
+vector<int> inorderTraversal(TreeNode* root);
+
+void inorderHelper(TreeNode* root, vector<int> &trav) {
+    if(root) {
+        if(root->left)
+            inorderHelper(root->left, trav);
+        trav.push_back(root->val);
+        if(root->right)
+            inorderHelper(root->right, trav);
+    }
+}
+
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> traversal;
+    inorderHelper(root, traversal);
+    return traversal;
+}
+
 int main() {
+    /*
+       1
+        \
+         2
+        /
+       3
+     Expected output: 1 3 2
+     */
+    TreeNode* tree1 = new TreeNode(1);
+    tree1->right = new TreeNode(2);
+    tree1->right->left = new TreeNode(3);
+    
+    vector<int> soln = inorderTraversal(tree1);
+    
+    for(int e : soln)
+        cout << e << ' ';
+    
     return 0;
 }
